f3b5a8c9bc9fd137abbd34046edc48a27cbf83bf
SamuelOgino <samueloginoam@gmail.com>
2025-11-18 23:52:31 -0400

    Add models.py code

---
+++ App/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from .database import Base
+import datetime
+
+class Tarefa(Base):
+    __tablename__ = "tarefas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String)
+    descricao = Column(String)
+    concluida = Column(Boolean, default=False)
+    data_limite = Column(DateTime, nullable=True)
+
+    data_criacao = Column(DateTime, default=datetime.datetime.now)
