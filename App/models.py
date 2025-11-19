dc9be2c80737aa5d12a90ae585931fe1bb1bb8fd
SamuelOgino <samueloginoam@gmail.com>
2025-11-19 00:00:01 -0400

    Add nullable=False for titulo and True for descricao

--- App/models.py
+++ App/models.py
@@ -6,8 +6,8 @@ class Tarefa(Base):
     __tablename__ = "tarefas"
 
     id = Column(Integer, primary_key=True, index=True)
-    titulo = Column(String)
-    descricao = Column(String)
+    titulo = Column(String, nullable=False)
+    descricao = Column(String, nullable=True)
     concluida = Column(Boolean, default=False)
     data_limite = Column(DateTime, nullable=True)
 
